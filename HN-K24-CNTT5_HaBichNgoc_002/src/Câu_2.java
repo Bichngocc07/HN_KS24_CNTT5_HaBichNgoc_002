c03b8204340d2435090892b03144c96fee19b66c
Bichngocc07 <ngoc01072006@gmail.com>
2026-03-05 10:25:45 +0700

    Add files via upload

--- HN-K24-CNTT5_HaBichNgoc_002/src/Câu_2.java
+++ HN-K24-CNTT5_HaBichNgoc_002/src/Câu_2.java
@@ -3,30 +3,40 @@ import java.util.*;
 public class Câu_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap chuoi: ");
-        String str = sc.nextLine();
-        if (str.trim().length() == 0) {
-            System.out.println("Lỗi: Chuỗi không được để trống.");
+
+        String s = sc.nextLine();
+
+        if (s.trim().length() == 0) {
+            System.out.println("Lỗi: Chuỗi không được để trống");
             return;
         }
 
-        str = str.toLowerCase();
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
+        s = s.toLowerCase();
+        s = s.replace(",", "");
+        s = s.replace(".", "");
 
-            if (c == ' ') continue;
+        String[] words = s.split(" ");
 
-            if (map.containsKey(c)) {
-                int count = map.get(c);
-                map.put(c, count + 1);
-            } else {
-                map.put(c, 1);
+        Map<Integer, Set<String>> map = new HashMap<>();
+
+        for (String w : words) {
+
+            int len = w.length();
+
+            if (!map.containsKey(len)) {
+                map.put(len, new HashSet<>());
             }
+
+            map.get(len).add(w);
         }
 
-        for (Character key : map.keySet()) {
-            System.out.println("Ky tu " + key + ": " + map.get(key) + " lan");
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+
+        System.out.println("Phân loại theo độ dài:");
+
+        for (int k : keys) {
+            System.out.println("- " + k + " ký tự: " + map.get(k));
         }
     }
 }
